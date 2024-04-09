175527a8791e8015a3eaac1d9b7280e8dd3d57b8
sakir456 <shaikhsakir1015@gmail.com>
2024-04-10 00:09:41 +0530

    getUserChannelProfile controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -269,7 +269,7 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 const getCurrentUser = asyncHandler(async(req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse (200, req.user, "current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req, res) => {
@@ -279,7 +279,7 @@ const updateAccountDetails = asyncHandler(async(req, res) => {
       throw new ApiError(400, "All fields are required")
     }
 
-   const user = User.findByIdAndUpdate(
+   const user = await User.findByIdAndUpdate(
       req?.user._id,
       {
         $set: {
@@ -356,8 +356,83 @@ const updateUserCoverImage = asyncHandler(async(req, res)=> {
   )
 })
 
+
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+      const {username} = req.params
+      if(!username?.trim()) {
+        throw new ApiError(400, "username is missing")
+      }
+
+      const channel = await User.aggregate([
+        {
+          $match: {
+            username:username?.toLowerCase()
+          }   
+       },
+       {
+        $lookup: {
+          from: "subscriptions", // here subscriptions comes from subscription.model.js it becomes subscriptions because when it saved it converts to lowercase and becomes plural
+          localField:"_id",
+          foreignField: "channel",
+          as: "subscribers"
+        }
+       },
+       {
+        $lookup: {
+          from: "subscriptions",
+          localField:"_id",
+          foreignField: "subscriber",
+          as: "subscribedTo"
+        }
+       },
+       {
+        $addFields: {
+          subscribersCount: {
+            $size: "$subscribers",
+          },
+          channelSubscribedToCount: {
+            $size: "$subscribedTo"
+          },
+          isSubscribed: {
+           $cond: {
+             if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+             then: true,
+             else: false
+           }
+          }
+
+        }
+       },
+       {
+        $project: {
+          fullname: 1,
+          username: 1,
+          subscribersCount: 1,
+          channelSubscribedToCount: 1,
+          isSubscribed: 1,
+          avatar: 1,
+          coverImage: 1,
+          email: 1,
+
+
+
+        }
+       }
+    ])
+
+    if (channel?.length) {
+      throw new ApiError(404, "channel does not exist")
+      }
+
+      return res
+      .status(200)
+      .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+      )
+})
+
 export { registerUser, loginUser, 
   logoutUser, refreshAccessToken,
    getCurrentUser, changeCurrentPassword, 
    updateAccountDetails, updateUserAvatar,
-   updateUserCoverImage }
+   updateUserCoverImage, getUserChannelProfile }
